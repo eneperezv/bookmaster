48c1371759438f81159323439b4b282cacaa00a7
eneperezv <eneperezv@gmail.com>
2024-08-14 02:23:30 -0500

    Se crean clases UI para control de Autores, Editoriales y Usuarios. Se modifica la descripcion del proyecto en el archivo POM.
     Changes to be committed:
    	modified:   bookmaster.api/pom.xml
    	new file:   bookmaster.client/src/main/java/com/eenp/bookmaster/client/ui/AuthorMain.java
    	new file:   bookmaster.client/src/main/java/com/eenp/bookmaster/client/ui/PublisherMain.java
    	new file:   bookmaster.client/src/main/java/com/eenp/bookmaster/client/ui/UserMain.java
    	modified:   bookmaster.client/target/classes/META-INF/maven/bookmaster/bookmaster.client/pom.properties

---
+++ bookmaster.client/src/main/java/com/eenp/bookmaster/client/ui/AuthorMain.java
@@ -0,0 +1,42 @@
+package com.eenp.bookmaster.client.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class AuthorMain extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AuthorMain frame = new AuthorMain();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AuthorMain() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
