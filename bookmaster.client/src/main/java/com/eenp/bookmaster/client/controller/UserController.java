5805c90d4b4f2ec9754aafaf312615f2020a0238
eneperezv <eneperezv@gmail.com>
2024-08-21 23:31:51 -0500

    Se incluye documentacion inicial para las clases.
     Changes to be committed:
    	modified:   bookmaster.client/src/main/java/com/eenp/bookmaster/client/controller/PublisherController.java
    	modified:   bookmaster.client/src/main/java/com/eenp/bookmaster/client/controller/UserController.java
    	modified:   bookmaster.client/src/main/java/com/eenp/bookmaster/client/ui/Start.java
    	modified:   bookmaster.client/target/classes/META-INF/maven/bookmaster/bookmaster.client/pom.properties

--- bookmaster.client/src/main/java/com/eenp/bookmaster/client/controller/UserController.java
+++ bookmaster.client/src/main/java/com/eenp/bookmaster/client/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.eenp.bookmaster.client.controller;
 
-import java.io.IOException;
-
 /*
  * @(#)UserController.java 1.0 07/08/2024
  * 
@@ -18,6 +16,8 @@ import java.io.IOException;
  * @version 1.0 | 07/08/2024
  * @since 1.0
  */
+
+import java.io.IOException;
 
 import java.net.URISyntaxException;
 
