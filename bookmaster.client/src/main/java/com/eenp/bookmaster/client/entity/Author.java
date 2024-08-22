fd77a89d937c6bc26a12a724482799162b803466
eneperezv <eneperezv@gmail.com>
2024-08-21 23:36:17 -0500

    Se incluye documentacion inicial de las clases
     Changes to be committed:
    	modified:   bookmaster.client/src/main/java/com/eenp/bookmaster/client/entity/Author.java
    	modified:   bookmaster.client/src/main/java/com/eenp/bookmaster/client/entity/Publisher.java
    	modified:   bookmaster.client/src/main/java/com/eenp/bookmaster/client/entity/Token.java

--- bookmaster.client/src/main/java/com/eenp/bookmaster/client/entity/Author.java
+++ bookmaster.client/src/main/java/com/eenp/bookmaster/client/entity/Author.java
@@ -1,5 +1,22 @@
 package com.eenp.bookmaster.client.entity;
 
+/*
+ * @(#)Author.java 1.0 12/08/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * Entidad para controlar los datos del Autores
+ *
+ * @author eliezer.navarro
+ * @version 1.0 | 12/08/2024
+ * @since 1.0
+ */
+
 public class Author {
 	
 	private Integer id;
--- bookmaster.client/src/main/java/com/eenp/bookmaster/client/entity/Publisher.java
+++ bookmaster.client/src/main/java/com/eenp/bookmaster/client/entity/Publisher.java
@@ -1,5 +1,22 @@
 package com.eenp.bookmaster.client.entity;
 
+/*
+ * @(#)Publisher.java 1.0 12/08/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * Entidad para controlar los datos de las Editoriales
+ *
+ * @author eliezer.navarro
+ * @version 1.0 | 12/08/2024
+ * @since 1.0
+ */
+
 public class Publisher {
 	
 	private Integer id;
