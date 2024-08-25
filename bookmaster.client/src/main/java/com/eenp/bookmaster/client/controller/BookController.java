b29b0823ca0ddbdf52c4bf54b5e2962acdb8e7cb
eneperezv <eneperezv@gmail.com>
2024-08-24 21:46:38 -0500

    Se inicializan clase controller y se agregan atributos de la clase book.
     Changes to be committed:
    	modified:   bookmaster.client/src/main/java/com/eenp/bookmaster/client/controller/BookController.java
    	modified:   bookmaster.client/src/main/java/com/eenp/bookmaster/client/entity/Book.java

--- bookmaster.client/src/main/java/com/eenp/bookmaster/client/controller/BookController.java
+++ bookmaster.client/src/main/java/com/eenp/bookmaster/client/controller/BookController.java
@@ -1,5 +1,13 @@
 package com.eenp.bookmaster.client.controller;
 
+import com.eenp.bookmaster.client.service.BookService;
+
 public class BookController {
+	
+	private final BookService bookService;
+	
+	public BookController() {
+		this.bookService = new BookService();
+	}
 
 }
--- bookmaster.client/src/main/java/com/eenp/bookmaster/client/entity/Book.java
+++ bookmaster.client/src/main/java/com/eenp/bookmaster/client/entity/Book.java
@@ -1,5 +1,61 @@
 package com.eenp.bookmaster.client.entity;
 
 public class Book {
+	
+	private Integer id;
+	private String titulo;
+	private Integer idautor;
+	private Author author;
+	private Integer ideditorial;
+	private Publisher publisher;
+	private Integer aniopublicacion;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public Integer getIdautor() {
+		return idautor;
+	}
+	public void setIdautor(Integer idautor) {
+		this.idautor = idautor;
+	}
+	public Author getAuthor() {
+		return author;
+	}
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+	public Integer getIdeditorial() {
+		return ideditorial;
+	}
+	public void setIdeditorial(Integer ideditorial) {
+		this.ideditorial = ideditorial;
+	}
+	public Publisher getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(Publisher publisher) {
+		this.publisher = publisher;
+	}
+	public Integer getAniopublicacion() {
+		return aniopublicacion;
+	}
+	public void setAniopublicacion(Integer aniopublicacion) {
+		this.aniopublicacion = aniopublicacion;
+	}
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", titulo=" + titulo + ", idautor=" + idautor + ", author=" + author
+				+ ", ideditorial=" + ideditorial + ", publisher=" + publisher + ", aniopublicacion=" + aniopublicacion
+				+ "]";
+	}
 
 }
