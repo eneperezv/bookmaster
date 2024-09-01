cbb058a7712d362c226ece36924dcdea313fcc08
eneperezv <eneperezv@gmail.com>
2024-08-31 23:41:14 -0500

    Se incluyen controles basicos de la interfaz devoluciones.
     Changes to be committed:
    	modified:   bookmaster.client/src/main/java/com/eenp/bookmaster/client/ui/ReturnsMain.java

--- bookmaster.client/src/main/java/com/eenp/bookmaster/client/ui/ReturnsMain.java
+++ bookmaster.client/src/main/java/com/eenp/bookmaster/client/ui/ReturnsMain.java
@@ -1,16 +1,44 @@
 package com.eenp.bookmaster.client.ui;
 
 import java.awt.EventQueue;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+
+import org.apache.http.ParseException;
+
+import com.eenp.bookmaster.client.util.Functions;
 
 public class ReturnsMain extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+    private JTable table;
+    private DefaultTableModel tableModel;
 
+	Functions func = new Functions();
+	private JButton btnGuardar;
+	private JButton btnSalir;
+	private JLabel lblNewLabel;
+	private JTextField txtCliente;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -31,12 +59,111 @@ public class ReturnsMain extends JFrame {
 	 * Create the frame.
 	 */
 	public ReturnsMain() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setTitle("BookMaster | 1.0 | Devoluciones");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setBounds(100, 100, 768, 500);
+        setLocationRelativeTo(null);
+
+        initialize();
+        try {
+			cargarDatosPrestamos();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void cargarDatosPrestamos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initialize() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Main.class.getResource("/com/eenp/bookmaster/client/images/book.png")));
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+
+        tableModel = new DefaultTableModel();
+        tableModel.addColumn("ID");
+        tableModel.addColumn("Autor");
+
+        table = new JTable(tableModel);
+        
+        TableColumnModel columnModel = table.getColumnModel();
+        
+        columnModel.getColumn(0).setMaxWidth(0);
+        columnModel.getColumn(0).setMinWidth(0);
+        columnModel.getColumn(0).setPreferredWidth(0);
+        
+        JScrollPane scrollPane = new JScrollPane(table);
+		
+		btnGuardar = new JButton(" Guardar");
+		btnGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+        		if(validarCampos()) {
+        			guardarInformacion();
+        		}
+			}
+		});
+		btnGuardar.setIcon(new ImageIcon(UserMain.class.getResource("/com/eenp/bookmaster/client/images/Save2.png")));
+		
+		btnSalir = new JButton(" Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				dispose();
+			}
+		});
+		btnSalir.setIcon(new ImageIcon(UserMain.class.getResource("/com/eenp/bookmaster/client/images/exit2.png")));
+		
+		lblNewLabel = new JLabel("Buscar por Cliente:");
+		
+		txtCliente = new JTextField();
+		txtCliente.setColumns(10);
+        
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(txtCliente, GroupLayout.PREFERRED_SIZE, 212, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 167, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED, 551, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnGuardar, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnSalir, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE)))
+				.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 894, Short.MAX_VALUE)
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblNewLabel)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(txtCliente, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(58))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(btnGuardar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnSalir, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)))
+					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 357, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+
+	private void guardarInformacion() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private boolean validarCampos() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
